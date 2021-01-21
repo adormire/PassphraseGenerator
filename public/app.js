632b6f51ffb29878cb0765a41862a4eaf209c4de
Andy Dormire <56885952+adormire@users.noreply.github.com>
2021-01-21 05:36:22 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,35 +2,58 @@ const desiredWordsPer = $('#wordsInPhrase');
 const desiredSetsGenerated = $('#desiredSetsGenerated');
 const resultsField = $('#passwordResults');
 const rollMax = 6;
+let generatedRolls = new Array();
+let matchedWords = new Array();
 
-function generate_passphrase() {
-  let wordsPerPhrase = desiredWordsPer.val();
-  let passphraseSets = desiredSetsGenerated.val();
-  let rollGroup = [];
+let wordList = new Array();
+$.get('eff_list.txt', function(data) {
+  wordList = data.split("\n");
+});
+
+function match_words(generatedRolls) {
+  for (let i = 0; i < generatedRolls.length; i++) {
+    console.log("Roll: " + generatedRolls[i]);
+    if ($.inArray(generatedRolls[i], wordList)) {
+      wordList.findIndex(function(entry) {
+        if (entry === generatedRolls[i]) {
+          console.log("Entry: " + entry);
+        }
+      });
+      console.log("match");
+    }
+  }
+}
+
+function generate_rolls() {
+  const wordsPerPhrase = desiredWordsPer.val();
+  const passphraseSets = desiredSetsGenerated.val();
 
   console.log("Words per: " + wordsPerPhrase);
   console.log("Sets generated: " + passphraseSets);
 
-  for (i = 0; i < passphraseSets; i++) {
-    for (j = 0; j < wordsPerPhrase; j++) {
-      for (k = 0; k < rollMax; k++) {
-        rollGroup.push(Math.floor((Math.random() * rollMax) + 1));
+  let rollGroup;
+  for (let i = 0; i < passphraseSets; i++) {
+    for (let j = 0; j < wordsPerPhrase; j++) {
+      rollGroup = '';
+      for (let k = 0; k < rollMax; k++) {
+        console.log(i, j, k);
+        rollGroup += (Math.floor((Math.random() * rollMax) + 1).toString());
       }
+      generatedRolls.push(rollGroup);
     }
   }
-  resultsField.text(rollGroup)
+  resultsField.text(generatedRolls);
+  match_words(generatedRolls);
 }
 
 $(function() {
-  const wordList = $.get('eff_list.txt', "text");
-
   desiredWordsPer.on('change', function() {
-    if (desiredSetsGenerated.val() != '')
-      generate_passphrase()
+    if (desiredSetsGenerated.val() !== '')
+      generate_rolls()
   })
 
   desiredSetsGenerated.on('change', function() {
-    if (desiredWordsPer.val() != '')
-      generate_passphrase()
+    if (desiredWordsPer.val() !== '')
+      generate_rolls()
   })
 });
